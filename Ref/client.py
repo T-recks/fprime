976e8c2ed27397d0fc30da7d926c0cc9f48531f6
Timothy Recker <timjrecker@gmail.com>
2023-04-02 12:00:04 -0700

    include client.py

---
+++ Ref/client.py
@@ -0,0 +1,14 @@
+import socket
+import struct
+localIP = ""
+# localPort = 4556
+localPort = 4558
+bufferSize = 1024
+msg = b'0x9f88070000820282020282028201018202820101820018281a000f424085010100005823526561647920746f2067656e657261746520612033322d62797465207061796c6f6164ff'
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((localIP, localPort))
+s.send(msg)
+# data = s.recv(bufferSize)
+s.close()
+print("sent")
+# print( "received data:", data)
